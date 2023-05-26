0fe908013f9ef43f06c86e4510618a47cf846afd
Steffen Schubert <steff.schubert@gmx.de>
2023-05-27 00:07:34 +0200

    Localstorage

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit'
 import {ModuleStateSlice} from "./state/ModulesStateSlice";
 
+// convert object to string and store in localStorage
+function saveToLocalStorage(state: RootState) {
+    try {
+        const serialisedState = JSON.stringify(state);
+        localStorage.setItem("persistantState", serialisedState);
+    } catch (e) {
+        console.warn(e);
+    }
+}
+
+// load string from localStarage and convert into an Object
+// invalid output must be undefined
+function loadFromLocalStorage() {
+    try {
+        const serialisedState = localStorage.getItem("persistantState");
+        if (serialisedState === null) return undefined;
+        return JSON.parse(serialisedState);
+    } catch (e) {
+        console.warn(e);
+        return undefined;
+    }
+}
+
 const store = configureStore({
     reducer: {
         modules: ModuleStateSlice.reducer,
     },
+    preloadedState: loadFromLocalStorage()
 });
+
+// listen for store changes and use saveToLocalStorage to
+// save them to localStorage
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store
 // Infer the `RootState` and `AppDispatch` types from the store itself
